8e1c5c9fa7d1ac898192933f6b8021c5544ec256
Rodney St Cloud <hoyrod1@aol.com>
2024-05-01 16:53:57 -0400

    All the form elements have been cached and event listeners have been attached to both registration for and login form

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 console.log(
   "======================= Form Validation Homework ======================="
 );
+//------------ Registration form elements cached ------------//
+const registrationForm = document.getElementById("registration");
+const regUserName = registrationForm.elements["username"];
+const regUserEmail = registrationForm.elements["email"];
+const regUserPassword = registrationForm.elements["password"];
+const checkPassword = registrationForm.elements["passwordCheck"];
+
+//------------ Event Listner for registration form ------------//
+registrationForm.addEventListener("submit", registrationFormSubmission);
+
+function registrationFormSubmission(e) {
+  e.preventDefault();
+  console.log(e);
+  console.log(regUserName.value);
+}
+//=============================================================//
+//---------------- Login form elements cached ----------------//
+const loginForm = document.getElementById("login");
+
+//------------ Event Listner for registration form ------------//
+loginForm.addEventListener("submit", loginFormSubmission);
+const loginUserName = loginForm.elements["username"];
+const loginUserPassword = loginForm.elements["password"];
+
+function loginFormSubmission(e) {
+  e.preventDefault();
+  console.log(e);
+}
