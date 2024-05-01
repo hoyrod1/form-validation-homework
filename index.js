cffece6d9b9b96f8ba2b7f1354ff503c7f15c7fe
Rodney St Cloud <hoyrod1@aol.com>
2024-05-01 17:44:32 -0400

    Converted the login username inputed to lower case

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 console.log(
   "======================= Form Validation Homework ======================="
 );
+//------------- Error display container cached -------------//
+let errorDisplay = document.getElementById("errorDisplay");
 //------------ Registration form elements cached ------------//
 const registrationForm = document.getElementById("registration");
 const regUserName = registrationForm.elements["username"];
 const regUserEmail = registrationForm.elements["email"];
 const regUserPassword = registrationForm.elements["password"];
 const checkPassword = registrationForm.elements["passwordCheck"];
+const acceptTerms = registrationForm.elements["terms"];
 
 //------------ Event Listner for registration form ------------//
 registrationForm.addEventListener("submit", registrationFormSubmission);
@@ -14,18 +17,22 @@ registrationForm.addEventListener("submit", registrationFormSubmission);
 function registrationFormSubmission(e) {
   e.preventDefault();
   console.log(e);
+  console.log(acceptTerms.value);
   console.log(regUserName.value);
 }
 //=============================================================//
 //---------------- Login form elements cached ----------------//
 const loginForm = document.getElementById("login");
-
-//------------ Event Listner for registration form ------------//
-loginForm.addEventListener("submit", loginFormSubmission);
 const loginUserName = loginForm.elements["username"];
 const loginUserPassword = loginForm.elements["password"];
+const keepMeLogin = loginForm.elements["persist"];
+
+//--------------- Event Listner for login form ---------------//
+loginForm.addEventListener("submit", loginFormSubmission);
 
 function loginFormSubmission(e) {
   e.preventDefault();
   console.log(e);
+  console.log(keepMeLogin.value);
+  console.log(loginUserName.value.toLowerCase());
 }
