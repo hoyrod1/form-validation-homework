101ceff8d3b1a9d58615ef6ae99725cd0f4c5af0
Rodney St Cloud <hoyrod1@aol.com>
2024-05-01 20:29:21 -0400

    Completed login validation for empty fields and to check if the password matches the password stored in local stoarge

--- index.js
+++ index.js
@@ -21,18 +21,75 @@ function registrationFormSubmission(e) {
   console.log(regUserName.value);
 }
 //=============================================================//
+
+//=============================================================//
+// Retrieving password from local storage
+const storedPassword = localStorage.getItem("password");
 //---------------- Login form elements cached ----------------//
 const loginForm = document.getElementById("login");
 const loginUserName = loginForm.elements["username"];
 const loginUserPassword = loginForm.elements["password"];
 const keepMeLogin = loginForm.elements["persist"];
-
 //--------------- Event Listner for login form ---------------//
 loginForm.addEventListener("submit", loginFormSubmission);
 
 function loginFormSubmission(e) {
   e.preventDefault();
-  console.log(e);
+  const userNameVal = validateLoginUsername();
+  const userPasswordVal = validateLoginPassword();
+
+  if (userNameVal === false) {
+    e.preventDefault();
+    e.returnValue = false;
+    return false;
+  }
+
+  if (userPasswordVal === false) {
+    e.preventDefault();
+    e.returnValue = false;
+    return false;
+  }
   console.log(keepMeLogin.value);
-  console.log(loginUserName.value.toLowerCase());
+  const pTag = document.createElement("p");
+  pTag.style.color = "red";
+  pTag.textContent = "You have succesfully Logged in";
+  errorDisplay.appendChild(pTag);
+
+  //===================================================//
+
+  //===================================================//
+  //------------ Login Validaiton Function ------------//
+  // Validation function for the username
+  function validateLoginUsername() {
+    let userNameValue = loginUserName.value.toLowerCase();
+    const storedPassword = "The Stored Password";
+    if (userNameValue === "") {
+      const pTag = document.createElement("p");
+      pTag.style.color = "red";
+      pTag.textContent = "Username Feild can not be empty";
+      errorDisplay.appendChild(pTag);
+      loginUserName.focus();
+      return false;
+    }
+  }
+  // Validation function for the password
+  function validateLoginPassword() {
+    let userPasswordValue = loginUserPassword.value;
+    if (userPasswordValue === "") {
+      const pTag = document.createElement("p");
+      pTag.style.color = "red";
+      pTag.textContent = "Password Feild can not be empty";
+      errorDisplay.appendChild(pTag);
+      loginUserPassword.focus();
+      return false;
+    }
+    // if (userPasswordValue !== storedPassword) {
+    //   loginUserPassword.focus();
+    //   const pTag = document.createElement("p");
+    //   pTag.style.color = "red";
+    //   pTag.textContent = "Incorrect Password";
+    //   errorDisplay.appendChild(pTag);
+    //   return false;
+    // }
+  }
 }
